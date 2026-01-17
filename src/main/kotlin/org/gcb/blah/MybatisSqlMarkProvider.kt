8507dcdae23efbb42a07cc1840ab68a479cfc887
Keyang Li <liky1995@hotmail.com>
2026-01-17 13:28:49 +0800

    fix bugs

--- src/main/kotlin/org/gcb/blah/MybatisSqlMarkProvider.kt
+++ src/main/kotlin/org/gcb/blah/MybatisSqlMarkProvider.kt
@@ -6,30 +6,27 @@ import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiExpression
-import com.intellij.psi.PsiExpressionList
 import com.intellij.psi.PsiLiteralExpression
 import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.PsiSearchHelper
 import com.intellij.psi.search.UsageSearchContext
-import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.xml.XmlTag
 
-class MybatisSqlMarkProvider: RelatedItemLineMarkerProvider() {
+class MybatisSqlMarkProvider : RelatedItemLineMarkerProvider() {
     override fun collectNavigationMarkers(
         element: PsiElement,
         result: MutableCollection<in RelatedItemLineMarkerInfo<*>>
     ) {
-        if (element !is XmlTag) {
+        if (!element.isMybatisDmlTag()) {
             return
         }
-        val tagName = element.name
-        if (tagName !in setOf("select", "insert", "delete", "update")) {
+        val sqlId = findSqlId(element as XmlTag)
+        if (sqlId.isNullOrBlank()) {
             return
         }
-        val sqlId = element.getAttributeValue("id") ?: return
         val project = element.project
+        // 工具类的类名
         val toolClassName = PluginSettingState.getInstance(project).toolClassName
         if (toolClassName.isEmpty()) {
             return
@@ -39,51 +36,82 @@ class MybatisSqlMarkProvider: RelatedItemLineMarkerProvider() {
             return
         }
         val iconBuilder = NavigationGutterIconBuilder.create(
-            AllIcons.Gutter.ImplementedMethod)
+            AllIcons.Gutter.ImplementedMethod
+        )
             .setTargets(targets)
         result.add(iconBuilder.createLineMarkerInfo(element))
     }
 
+    private fun PsiElement.isMybatisDmlTag(): Boolean {
+        if (this !is XmlTag) {
+            return false
+        }
+        val tagName = this.name
+        if (tagName !in setOf("select", "insert", "delete", "update")) {
+            return false
+        }
+        return true
+    }
+
+
+    private fun findSqlId(mybatisDmlTag: XmlTag): String? {
+        val sqlId = mybatisDmlTag.getAttributeValue("id")
+        val ns = mybatisDmlTag.parent?.run { if (this is XmlTag && this.name == "mapper") return this.getAttributeValue("namespace") }
+        if (ns == null) {
+            return null
+        }
+        return "${ns}.${sqlId}"
+    }
+
+    private fun findLiteralAndItsUsage(
+        literal: PsiLiteralExpression,
+        toolClassName: String,
+        sqlId: String
+    ): List<PsiMethodCallExpression> {
+        TODO("Not yet implemented")
+    }
+
     /**
-     * 根据字符串找到sqlId的使用者
-     * 1. sqlId出现在一个函数调用中
+     * 根据字符串找到sqlId的使用函数
+     * 1. sqlId作为字符串的一部分出现在一个函数调用中
      * e.g. DBUtils.selectList(SQL_NS + "sqlId", ..)
-     * 2. sqlId出现在一个常量的赋值语句中
+     *
+     * 2. sqlId经过拼接后作为完整的sqlId
      * e.g. private static final String SQL_ID = SQL_NS + "sqlId"
      *      DBUtils.selectList(SQL_ID, ...)
+     *
+     * 3. sqlId直接属于字符串的一部分
+     *  e.g. private static final String SQL_ID = "sn+sqlId"
+     *       DBUtils.selectList(SQL_ID, ...)
      */
-    fun findMethod(project: Project, targetClassName:String, sqlId: String):List<PsiElement> {
+    fun findMethod(project: Project, toolClassName: String, sqlId: String): List<PsiElement> {
         val res = mutableListOf<PsiElement>()
-        val scope =  GlobalSearchScope.allScope(project)
+        val scope = GlobalSearchScope.allScope(project)
         val psiSearchHelper = PsiSearchHelper.getInstance(project)
-        psiSearchHelper.processElementsWithWord({
-            psiEl, _ ->
+        psiSearchHelper.processElementsWithWord({ psiEl, _ ->
             if (psiEl.parent !is PsiLiteralExpression) {
                 return@processElementsWithWord true
             }
             val literal = psiEl.parent as PsiLiteralExpression
-            if (literal.value != sqlId) {
-                return@processElementsWithWord true
+            val trivialLiteralAndItsUsages = findLiteralAndItsUsage(literal, toolClassName, sqlId);
+            if (trivialLiteralAndItsUsages.isNotEmpty()) {
+                res.addAll(trivialLiteralAndItsUsages)
             }
-            val expressionList = PsiTreeUtil.getParentOfType(literal, PsiExpressionList::class.java)
-                ?: return@processElementsWithWord true
-            val expressions = expressionList.expressions
-            if (expressions.isEmpty() || expressions[0] != literal) {
-                return@processElementsWithWord true
-            }
-            val methodCall = PsiTreeUtil.getParentOfType(expressionList, PsiMethodCallExpression::class.java)
-                ?: return@processElementsWithWord true
-            val qn = methodCall.resolveMethod()?.containingClass?.qualifiedName
-            if (qn == targetClassName) {
-                res.add(methodCall)
+            val binaryExpressionAndItsUsages = findBinaryExpressionAndItsUsageWhenLiteralRefSqlId(literal, toolClassName, sqlId)
+            if (binaryExpressionAndItsUsages.isNotEmpty()) {
+                res.addAll(binaryExpressionAndItsUsages)
             }
             true
         }, scope, sqlId, UsageSearchContext.IN_STRINGS, true)
         return res
     }
 
-
-    fun checkIsExprSqlUsage(expr: PsiExpression): Boolean {
-        return false
+    private fun findBinaryExpressionAndItsUsageWhenLiteralRefSqlId(
+        literal: PsiLiteralExpression,
+        toolClassName: String,
+        sqlId: String
+    ): List<PsiMethodCallExpression> {
+        return listOf()
     }
+
 }
